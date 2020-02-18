9259ea45f7c7a273b076c9fae67fad912be90307
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-02-18 21:45:10 +0000

    Fmt and updated API to avoid allocation

--- examples/tictactoe.rs
+++ examples/tictactoe.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use turk::{Player, GameState};
+use turk::{GameState, Player};
 
 #[derive(Clone)]
 struct TicTacToe {
@@ -10,23 +10,25 @@ struct TicTacToe {
 impl GameState for TicTacToe {
     type Move = (usize, usize);
 
-    fn next_player(&self) -> Player { self.next_player }
+    fn next_player(&self) -> Player {
+        self.next_player
+    }
 
     fn apply_move(&mut self, (x, y): Self::Move) {
         self.board[x][y] = Some(self.next_player);
         self.next_player = !self.next_player;
     }
 
-    fn get_moves(&self) -> Vec<Self::Move> {
+    fn for_each_move<F: FnMut(Self::Move)>(&self, f: F) {
         if self.eval_score() == 0 {
             (0..3)
-                .map(|x| (0..3)
-                    .filter(move |y| self.board[x][*y].is_none())
-                    .map(move |y| (x, y)))
+                .map(|x| {
+                    (0..3)
+                        .filter(move |y| self.board[x][*y].is_none())
+                        .map(move |y| (x, y))
+                })
                 .flatten()
-                .collect()
-        } else {
-            Vec::new()
+                .for_each(f);
         }
     }
 
@@ -35,7 +37,7 @@ impl GameState for TicTacToe {
         for i in 0..3 {
             match (self.board[i][0], self.board[i][1], self.board[i][2]) {
                 (Some(x), Some(y), Some(z)) if x == y && y == z => return 1,
-                _ => {},
+                _ => {}
             }
         }
 
@@ -43,18 +45,18 @@ impl GameState for TicTacToe {
         for i in 0..3 {
             match (self.board[0][i], self.board[1][i], self.board[2][i]) {
                 (Some(x), Some(y), Some(z)) if x == y && y == z => return 1,
-                _ => {},
+                _ => {}
             }
         }
 
         // Diagonals
         match (self.board[0][0], self.board[1][1], self.board[2][2]) {
             (Some(x), Some(y), Some(z)) if x == y && y == z => return 1,
-            _ => {},
+            _ => {}
         }
         match (self.board[2][0], self.board[1][1], self.board[0][2]) {
             (Some(x), Some(y), Some(z)) if x == y && y == z => return 1,
-            _ => {},
+            _ => {}
         }
 
         0
@@ -72,9 +74,27 @@ impl fmt::Display for TicTacToe {
         }
 
         writeln!(f, ",-----,")?;
-        writeln!(f, "|{} {} {}|", p2c(self.board[0][0]), p2c(self.board[1][0]), p2c(self.board[2][0]))?;
-        writeln!(f, "|{} {} {}|", p2c(self.board[0][1]), p2c(self.board[1][1]), p2c(self.board[2][1]))?;
-        writeln!(f, "|{} {} {}|", p2c(self.board[0][2]), p2c(self.board[1][2]), p2c(self.board[2][2]))?;
+        writeln!(
+            f,
+            "|{} {} {}|",
+            p2c(self.board[0][0]),
+            p2c(self.board[1][0]),
+            p2c(self.board[2][0])
+        )?;
+        writeln!(
+            f,
+            "|{} {} {}|",
+            p2c(self.board[0][1]),
+            p2c(self.board[1][1]),
+            p2c(self.board[2][1])
+        )?;
+        writeln!(
+            f,
+            "|{} {} {}|",
+            p2c(self.board[0][2]),
+            p2c(self.board[1][2]),
+            p2c(self.board[2][2])
+        )?;
         writeln!(f, "'-----'")?;
         Ok(())
     }
@@ -87,13 +107,13 @@ fn main() {
     };
 
     loop {
-        if let Some(mov) = board.solve_depth(Player::One, 10) {
+        if let Some(mov) = board.solve_depth(Player::One, 8) {
             println!("{}", board);
             board.apply_move(mov);
         } else {
             break;
         }
-        if let Some(mov) = board.solve_depth(Player::Two, 10) {
+        if let Some(mov) = board.solve_depth(Player::Two, 8) {
             println!("{}", board);
             board.apply_move(mov);
         } else {
--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ pub trait GameState: Clone {
 
     fn next_player(&self) -> Player;
     fn apply_move(&mut self, mov: Self::Move);
-    fn get_moves(&self) -> Vec<Self::Move>;
+    fn for_each_move<F: FnMut(Self::Move)>(&self, f: F);
     fn eval_score(&self) -> i32;
 
     fn solve_depth(&self, player: Player, depth: usize) -> Option<Self::Move> {
@@ -30,7 +30,8 @@ pub trait GameState: Clone {
             self,
             if self.next_player() == player { -1 } else { 1 },
             depth,
-        ).1
+        )
+        .1
     }
 }
 
@@ -38,14 +39,21 @@ fn min<G: GameState>(game: &G, flip: i32, depth: usize) -> (i32, Option<G::Move>
     if depth == 0 {
         (game.eval_score(), None)
     } else {
-        game.get_moves()
-            .into_iter()
-            .map(|mov| {
-                let mut this = game.clone();
-                this.apply_move(mov.clone());
-                (flip * min(&this, -1, depth - 1).0, Some(mov))
-            })
-            .min_by(|(a, _), (b, _)| a.cmp(b))
-            .unwrap_or_else(|| (game.eval_score(), None))
+        let mut min_mov = None;
+
+        game.for_each_move(|mov| {
+            let mut this = game.clone();
+            this.apply_move(mov.clone());
+            let cost = flip * min(&this, -1, depth - 1).0;
+            if min_mov
+                .as_ref()
+                .map(|(_, c): &(G::Move, i32)| cost > *c)
+                .unwrap_or(true)
+            {
+                min_mov = Some((mov, cost));
+            }
+        });
+
+        (game.eval_score(), min_mov.map(|(mov, _)| mov))
     }
 }
